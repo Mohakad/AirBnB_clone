eb3132351f09d2f74c0857562f8cfe66ef6f1098
Eseteselase <139978253+Eseteselase@users.noreply.github.com>
2024-02-11 23:44:35 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,37 +2,46 @@
 """serializes and deserializes"""
 import json
 from models.base_model import BaseModel
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.user import User
 
 
 class FileStorage:
     """serializes instances and deserializes"""
     __file_path = "file.json"
     __objects = {}
+    class_dict = {"BaseModel": BaseModel, "User": User, "State": State,
+                  "Amenity": Amenity, "Place": Place, "City": City,                       "Review": Review}
 
     def all(self):
         """return the dictionary"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets with key id"""
-        FileStorage.__objects["{}.{}".format(obj.__class__.__name__,
-                                             obj.id)] = obj
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        obfile = FileStorage.__objects
-        obdict = {obj: obfile[obj].to_dict() for obj in obfile.keys()}
-        with open(FileStorage.__file_path, "w") as savef:
-            json.dump(obdict, savef)
+        my_dic = {}
+        for key, obj in self.__objects.items():
+            my_dic[key] = obj.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(my_dic, f)
 
     def reload(self):
         """deserialize JSON file"""
         try:
-            with open(FileStorage.__file_path) as jfl:
-                jdict = json.load(jfl)
-                for data in jdict.values():
-                    cname = data["__class__"]
-                    del data["__class__"]
-                    self.new(eval(cname)(**data))
-        except OSError:
+            with open(self.__file_path, "r") as f:
+                new_obj = json.load(f)
+            for key, val in new_obj.items():
+                obj = self.class_dict[val['__class__']](**val)
+                self.__objects[key] = obj
+        except FileNotFoundError:
             pass
