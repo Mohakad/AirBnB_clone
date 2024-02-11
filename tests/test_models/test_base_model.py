5786523391196839218751984643c47262411322
Mohakad <mohkadtrd@gmail.com>
2024-02-11 22:42:07 +0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -28,5 +28,26 @@ class test_init(unittest.TestCase):
         self.assertIn("'id': '111111'", bmstr)
         self.assertIn("'created_at': " + dtr, bmstr)
         self.assertIn("'updated_at': " + dtr, bmstr)
+    def test_kwargsini(self):
+        dtt = datetime.now()
+        dat_is = dtt.isoformat()
+        bsm = BaseModel(id="911", created_at=dat_is, updated_at=dat_is)
+        self.assertEqual(bsm.id, "911")
+        self.assertEqual(bsm.created_at, dtt)
+        self.assertEqual(bsm.updated_at, dtt)
+    def test_no_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+    def test_unused_arg(self):
+        bsm = BaseModel(None)
+        self.assertNotIn(None, bsm.__dict__.values())
+    def test_no_arg(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+    def test_c_at_public(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+    def test_updat_public(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+    
+
 if __name__ == "__main__":
     unittest.main()
